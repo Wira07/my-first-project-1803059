f258f4faec5918787767cf4041a64b9dbbe91031
wiralodrasaputra07 <wiralodrasaputra07@gmail.com>
2024-06-05 03:01:38 +0000

    first flutter me

---
+++ lib/main_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: const Color.fromARGB(255, 0, 0, 0),
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple.shade100, 
+        title: Text('IDX Demo', style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      body: const Center(
+        child: Text('Wira Sukma Saputra'),
+      ),
+    );
+  }
+}
